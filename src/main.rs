64080cd0f0c31599b04eaff07b354f51b4736753
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-10-01 16:18:24 -0400

    add logging crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::{info, LevelFilter};
 use num::{BigUint, FromPrimitive, One};
 use std::fs::File;
 use std::io::{BufWriter, Write};
@@ -6,6 +7,11 @@ use std::thread;
 use std::{env, process::exit};
 
 fn main() {
+    // Initialize logger
+    env_logger::builder()
+        .filter_level(LevelFilter::Trace)
+        .init();
+
     // Parse command line arguments for FACTORIAL and NUM_THREADS
     let args: Vec<String> = env::args().collect();
 
@@ -60,9 +66,8 @@ fn main() {
                 // Update the progress counter within the Mutex
                 *progress += 1;
 
-                // Print progress
-                if *progress % 1000 == 0 {
-                    println!("{}/{}", *progress, factorial);
+                if *progress % 10000 == 0 {
+                    info!("{}/{}", *progress, factorial);
                 }
             }
             result
@@ -74,10 +79,10 @@ fn main() {
     // Wait for all threads to finish and collect their results
     for (i, handle) in handles.into_iter().enumerate() {
         results.push(handle.join().unwrap());
-        println!("Thread {} finished", i + 1);
+        info!("Thread {} finished", i + 1);
     }
 
-    println!("Combining results from threads...");
+    info!("Combining results from threads...");
 
     // Initialize final result
     let mut final_result = BigUint::one();
@@ -85,11 +90,11 @@ fn main() {
     // Combine the results from each thread into the final result
     for (i, result) in results.into_iter().enumerate() {
         final_result *= result;
-        println!("{}/{}", i + 1, num_threads);
+        info!("{}/{}", i + 1, num_threads);
     }
 
-    println!("Final result is {}B", final_result.to_bytes_be().len());
-    println!("Converting to string...");
+    info!("Final result is {}B", final_result.to_bytes_be().len());
+    info!("Converting to string...");
 
     // Convert final result to string
     let final_result_str = final_result.to_string();
@@ -97,7 +102,7 @@ fn main() {
     // Create file name
     let file_name = format!("{}.txt", factorial);
 
-    println!("Writing to {}...", file_name);
+    info!("Writing to {}...", file_name);
 
     // Create a buffered file writer
     let mut writer = BufWriter::new(File::create(&file_name).unwrap());
@@ -108,5 +113,5 @@ fn main() {
     // Ensure that all buffered data is written
     writer.flush().unwrap();
 
-    println!("Factorial written to {}", file_name);
+    info!("Factorial written to {}", file_name);
 }
